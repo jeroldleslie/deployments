ac100cac6f370f8aad35376ddd10fdef0813e73b
KingOfPoptart <richard.c.duarte@gmail.com>
2015-05-28 14:19:13 +0100

    Adding neverwinterdp_home option to integration tests

--- tests/scribengin/integration-tests/Scribengin_Integration_S3_To_S3.sh
+++ tests/scribengin/integration-tests/Scribengin_Integration_S3_To_S3.sh
@@ -1,6 +1,12 @@
+#SCRIPT_DIR is the directory this script is in.  Allows us to execute without relative paths
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#This Script will set root directory and Neverwinter_home
+source $SCRIPT_DIR/setupEnvironment.sh $@
+
 #Set up docker images
-DOCKERSCRIBEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-$DOCKERSCRIBEDIR/../docker.sh cluster --clean-containers --run-containers --deploy-scribengin --start-cluster
+$ROOT/docker/scribengin/docker.sh cluster --clean-containers --run-containers --deploy-scribengin --start-cluster --neverwinterdp-home=$NEVERWINTER_HOME
+
 scp -o "StrictHostKeyChecking no" -r /root/.aws neverwinterdp@hadoop-master:/home/neverwinterdp/
 
 #make folder for test results
@@ -64,4 +70,4 @@ ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengi
 scp -o stricthostkeychecking=no neverwinterdp@hadoop-master:/opt/scribengin/scribengin/S3_IntegrationTest.xml ./testresults/
 
 #Clean up
-$DOCKERSCRIBEDIR/.././docker.sh cluster --clean-containers
+$ROOT/docker/scribengin/docker.sh cluster --clean-containers --neverwinterdp-home=$NEVERWINTER_HOME
---
+++ tests/scribengin/integration-tests/setupEnvironment.sh
@@ -0,0 +1,28 @@
+function usage(){
+  echo "  Usage: "
+  echo "       ./integrationScript.sh /path/to/NeverwinterDP/"
+  echo "  Or you can set an environment variable:"
+  echo "       export NEVERWINTERDP_HOME=/path/to/NeverwinterDP/"
+  echo "       ./integrationScript.sh"
+}
+
+NEVERWINTER_HOME=$1
+
+#If the command line arg isn't passed in,
+#then get neverwinterdp_home from environment variable
+if [ "$NEVERWINTER_HOME" = "" ] ; then
+  NEVERWINTER_HOME="$NEVERWINTERDP_HOME"
+fi
+
+if [ ! -d $NEVERWINTER_HOME ] ; then
+  echo "$NEVERWINTER_HOME is not a valid directory!"
+  usage
+  exit
+elif [ "$NEVERWINTER_HOME" = "" ] ; then
+  echo "NEVERWINTERDP_HOME is not set!"
+  usage
+  exit
+fi
+
+
+ROOT=$( dirname $( dirname $( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))))
