7254bf46e9b81f62dbeb6d0b44c04a52aaf9e337
artfullyContrived <anthony.musyoki@gmail.com>
2015-06-05 11:02:02 +0300

    Slightly Increase number of messages for testing purpose.

--- tests/scribengin/integration-tests/Kafka_Stability_Integration__KafkaFailureSimulator_with_spare.sh
+++ tests/scribengin/integration-tests/Kafka_Stability_Integration__KafkaFailureSimulator_with_spare.sh
@@ -37,11 +37,11 @@ MONITOR_PID=$!
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin/tools/kafka/ &&     \
                               ./kafka.sh test stability --zk-connect zookeeper-1:2181 --topic stabilitytest      \
                               --replication 3 --send-period 0 --send-writer-type ack --send-max-duration 18000 \
-                              --send-max-per-partition 2000 --producer:message.send.max.retries=5            \
+                              --send-max-per-partition 20000 --producer:message.send.max.retries=5            \
                               --producer:retry.backoff.ms=100 --producer:queue.buffering.max.ms=1000             \
                               --producer:queue.buffering.max.messages=15000                                      \
                               --producer:topic.metadata.refresh.interval.ms=-1 --producer:batch.num.messages=100 \
-                              --producer:acks=all --producer:compression.type=gzip --consume-max 10000       \
+                              --producer:acks=all --producer:compression.type=gzip --consume-max 100000       \
                               --consume-max-duration 6000                                                     \
                               --junit-report junit-reports/KafkaMessageCheckTool.xml"
 
