4e8f0856c663a6920f5774293c5134e296680796
artfullyContrived <anthony.musyoki@gmail.com>
2015-06-22 10:45:39 +0300

    Adds ansible runner.

--- digitalocean/scribengin/ansiblerunner.py
+++ digitalocean/scribengin/ansiblerunner.py
@@ -1,49 +1,49 @@
-#!/usr/bin/python 
-import ansible.runner
-import ansible.playbook
-import ansible.inventory
+from ansible.playbook import PlayBook
+from ansible.inventory import Inventory
 from ansible import callbacks
 from ansible import utils
-import json
 
-### setting up the inventory
+import jinja2
+from tempfile import NamedTemporaryFile
+import os
 
-## first of all, set up a host (or more)
-example_host = ansible.inventory.host.Host(
-    name = '10.25.35.103',
-    port = 22
-    )
-# with its variables to modify the playbook
-example_host.set_variable( 'var', 'foo')
-
-## secondly set up the group where the host(s) has to be added
-example_group = ansible.inventory.group.Group(
-    name = 'sample_group_name'
-    )
-example_group.add_host(example_host)
-
-## the last step is set up the invetory itself
-example_inventory = ansible.inventory.Inventory()
-example_inventory.add_group(example_group)
-example_inventory.subset('sample_group_name')
-
-# setting callbacks
-stats = callbacks.AggregateStats()
+# Boilerplace callbacks for stdout/stderr and log output
+utils.VERBOSITY = 0
 playbook_cb = callbacks.PlaybookCallbacks(verbose=utils.VERBOSITY)
+stats = callbacks.AggregateStats()
 runner_cb = callbacks.PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY)
 
-# creating the playbook instance to run, based on "test.yml" file
-pb = ansible.playbook.PlayBook(
-    playbook = "../test.yml",
-    stats = stats,
-    callbacks = playbook_cb,
-    runner_callbacks = runner_cb,
-    inventory = example_inventory,
-    check=True
-    )
+# Dynamic Inventory
+inventory = """
+[zookeeper]
+{{ public_ip_address }}
+"""
 
-# running the playbook
-pr = pb.run()  
+inventory_template = jinja2.Template(inventory)
+rendered_inventory = inventory_template.render({
+    'public_ip_address': '45.55.30.170',
+})
 
-# print the summary of results for each host
-print json.dumps(pr, sort_keys=True, indent=4, separators=(',', ': '))
+# Create a temporary file and write the template string to it
+hosts = NamedTemporaryFile(delete=False)
+hosts.write(rendered_inventory)
+hosts.close()
+
+pb = PlayBook(
+    playbook='../../ansible/test.yml',
+    host_list=hosts.name,     # Our hosts, the rendered inventory file
+    callbacks=playbook_cb,
+    runner_callbacks=runner_cb,
+    stats=stats,
+    extra_vars={'neverwinterdp_home_override':'/home/anto/github/artfullyContrived/NeverwinterDP'}
+)
+
+results = pb.run()
+
+# Ensure on_stats callback is called
+# for callback modules
+playbook_cb.on_stats(pb.stats)
+
+os.remove(hosts.name)
+
+print results
