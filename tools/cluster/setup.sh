2f7eafa6f5be407fce8b7f70613109d189457562
jeroldleslie <jeroldleslie@gmail.com>
2015-07-09 05:57:10 -0400

    issue in kibana deployment fixed

--- tools/cluster/setup.sh
+++ tools/cluster/setup.sh
@@ -4,10 +4,10 @@ if [ -n "$(which apt-get)" ] ; then
   sudo apt-get install python-dev build-essential gcc -y --fix-missing
   sudo apt-get install libffi6 libffi-dev libssl-dev -y
   #TODO need to find a different way to install npm
-  sudo apt-add-repository ppa:chris-lea/node.js -y
-  sudo apt-get update
-  sudo apt-get install nodejs -y
-  sudo apt-get install npm -y
+  #sudo apt-add-repository ppa:chris-lea/node.js -y
+  #sudo apt-get update
+  #sudo apt-get install nodejs -y
+  #sudo apt-get install npm -y
 fi
 
 if [ -n "$(which yum)" ] ; then
@@ -22,4 +22,4 @@ wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 #Install required modules
 sudo easy_install --upgrade nose==1.3.4 tabulate paramiko junit-xml click requests pip
 sudo pip install pyopenssl==0.15.1 ndg-httpsclient pyasn1 kazoo elasticsearch python-digitalocean --upgrade
-sudo npm install elasticdump -g -y
+#sudo npm install elasticdump -g -y
