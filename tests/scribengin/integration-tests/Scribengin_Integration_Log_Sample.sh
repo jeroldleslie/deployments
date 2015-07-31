3c4943eca676f815c6c47ea592a9d9a851d39f42
Tuan Nguyen <tuan08@gmail.com>
2015-07-31 09:32:54 -0400

    update script

--- tests/scribengin/integration-tests/Scribengin_Integration_Log_Sample.sh
+++ tests/scribengin/integration-tests/Scribengin_Integration_Log_Sample.sh
@@ -11,6 +11,7 @@ ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/neverwint
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "chmod +x /opt/neverwinterdp/dataflow/log-sample/bin/*.sh"
 
 
-RUN_OPTS= "--profile=performance --storage=kafka --dedicated-executor=false --num-of-message=1000000 --message-size=512"
-ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master  "cd /opt/neverwinterdp &&  time ./dataflow/log-sample/bin/run.sh $RUN_OPTS"
+#RUN_OPTS="--profile=performance --storage=s3 --dedicated-executor=false --num-of-message=1000000 --message-size=512"
+RUN_OPTS="--profile=dataflow-worker-failure --kill-max=5 --kill-period=180000 --storage=hdfs --dedicated-executor=false --num-of-message=10000000 --message-size=512"
 
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master  "cd /opt/neverwinterdp &&  time ./dataflow/log-sample/bin/run.sh $RUN_OPTS"
