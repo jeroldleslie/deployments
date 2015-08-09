c9678a3d6952426a752207d56fe394bb8e0dafd1
Tuan Nguyen <tuan08@gmail.com>
2015-08-09 09:30:41 -0400

    update Log Sample script

--- tests/scribengin/integration-tests/Log_Sample_Splitter.sh
+++ tests/scribengin/integration-tests/Log_Sample_Splitter.sh
@@ -12,7 +12,5 @@ ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "chmod +x /opt/nev
 
 
 #RUN_OPTS="--profile=performance --storage=kafka --dedicated-executor=false --num-of-message=1000000 --message-size=512"
-RUN_OPTS="--profile=dataflow-worker-failure --execution-max=3 --execution-period=180000 --storage=kafka --dedicated-executor=false --num-of-message=3000000 --message-size=512"
-#RUN_OPTS="--profile=dataflow-start-stop --execution-max=3 --execution-period=90000 --storage=kafka --dedicated-executor=false --num-of-message=3000000 --message-size=512"
 
-ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master  "cd /opt/neverwinterdp &&  time ./dataflow/log-sample/bin/run-chain.sh $RUN_OPTS"
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master  "cd /opt/neverwinterdp &&  time ./dataflow/log-sample/bin/run-splitter.sh"
