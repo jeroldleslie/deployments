6d2f1ce1428ad0bf4b40fd185b1fb613b9c07e5b
Tuan Nguyen <tuan08@gmail.com>
2015-09-09 04:32:59 -0400

    add performance test

---
+++ tests/scribengin/performance/LogSampleChainPerformanceCluster.sh
@@ -0,0 +1,17 @@
+#SCRIPT_DIR is the directory this script is in.  Allows us to execute without relative paths
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#This Script will set root directory and Neverwinter_home
+source $SCRIPT_DIR/setupEnvironment.sh $@
+
+
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/neverwinterdp && ./scribengin/bin/shell.sh scribengin info"
+
+
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "chmod +x /opt/neverwinterdp/dataflow/log-sample/bin/*.sh"
+
+
+ENV_OPTS="--profile=performance --storage=kafka --dedicated-executor=false"
+DATAFLOW_OPTS="--num-of-worker=3 --num-of-executor-per-worker=3 --num-of-message=10000000 --message-size=512"
+
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master  "cd /opt/neverwinterdp &&  time ./dataflow/log-sample/bin/run-chain.sh $ENV_OPTS $DATAFLOW_OPTS"
---
+++ tests/scribengin/performance/test-small-cluster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NEVERWINTERDP_HOME="/home/neverwinterdp/NeverwinterDP"
+NEVERWINTERDP_DEPLOYMENTS_HOME="/home/neverwinterdp/neverwinterdp-deployments"
+
+$NEVERWINTERDP_DEPLOYMENTS_HOME/tools/cluster/clusterCommander.py digitalocean \
+  --launch --neverwinterdp-home $NEVERWINTERDP_HOME \
+  --create-containers $NEVERWINTERDP_DEPLOYMENTS_HOME/tools/cluster/digitalOceanConfigs/scribenginSmall.yml --subdomain smallcluster
+
+
+$NEVERWINTERDP_DEPLOYMENTS_HOME/tests/scribengin/performance/LogSampleChainSmallCluster.sh \
+  --stop --clean --build --deploy --start --profile-type=small 2>&1 | tee ~/log-sample-small-cluster-10Mx512.txt
