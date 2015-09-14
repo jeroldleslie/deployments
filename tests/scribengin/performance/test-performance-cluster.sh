8bee6db859095f168cc5888326af5c9dd0d24cad
Tuan Nguyen <tuan08@gmail.com>
2015-09-14 03:40:56 -0400

    update the script params

--- tests/scribengin/performance/test-performance-cluster.sh
+++ tests/scribengin/performance/test-performance-cluster.sh
@@ -7,7 +7,7 @@ $NEVERWINTERDP_DEPLOYMENTS_HOME/tools/cluster/clusterCommander.py digitalocean \
   --launch --neverwinterdp-home $NEVERWINTERDP_HOME \
   --create-containers $NEVERWINTERDP_DEPLOYMENTS_HOME/tools/cluster/digitalOceanConfigs/scribenginPerformance.yml --subdomain perfcluster
 
-#LogSampleChainPerformanceCluster.sh --stop --clean --build --deploy --restart --profile-type=performance 2>&1 | tee ~/log-sample-performance-cluster-10Mx512.txt
+#LogSampleChainPerformanceCluster.sh --stop --clean --build --deploy --restart --profile-type=performance 2>&1 | tee ~/log-sample-10Mx512.txt
 
-$NEVERWINTERDP_DEPLOYMENTS_HOME/tests/scribengin/performance/LogSampleChainPerformanceCluster.sh \
-  --stop --clean --build --deploy --start --profile-type=performance 2>&1 | tee ~/log-sample-performance-cluster-10Mx512.txt
+#$NEVERWINTERDP_DEPLOYMENTS_HOME/tests/scribengin/performance/LogSampleChainPerformanceCluster.sh \
+#  --stop --clean --build --deploy --start --profile-type=performance 2>&1 | tee ~/log-sample-10Mx512.txt
