f4512a677f34acf92605dc964e16a37b9da5c06f
Tuan Nguyen <tuan08@gmail.com>
2015-10-08 04:14:38 -0400

    fix configuration

--- tests/scribengin/stability2/LogSampleChainStabilityCluster.sh
+++ tests/scribengin/stability2/LogSampleChainStabilityCluster.sh
@@ -11,9 +11,15 @@ ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/neverwint
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "chmod +x /opt/neverwinterdp/dataflow/log-sample/bin/*.sh"
 
 
+
+GENERATOR_OPTS="--generator-max-wait-time=60000 --generator-send-period=1"
+
+#VALIDATOR_OPTS="--validator-disable"
+
 STORAGE_OPTS="--storage=kafka"
-DATAFLOW_OPTS="--dedicated-executor=false --num-of-worker=2 --num-of-executor-per-worker=2 --num-of-stream=8 --num-of-message=10000000 --message-size=512"
 #KILL_WORKER_OPTS="--kill-worker-random=true --kill-worker-period=120000 --kill-worker-max=30"
+DATAFLOW_OPTS="$STORAGE_OPTS --dedicated-executor=false --num-of-worker=2 --num-of-executor-per-worker=2 --num-of-stream=8 --num-of-message=150000000 --message-size=512 $KILL_WORKER_OPTS"
 
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master \
-   "cd /opt/neverwinterdp &&  time ./dataflow/log-sample/bin/run-dataflow-chain.sh $STORAGE_OPTS $DATAFLOW_OPTS $KILL_WORKER_OPTS"
+   "cd /opt/neverwinterdp &&  time ./dataflow/log-sample/bin/run-dataflow-chain.sh $GENERATOR_OPTS $DATAFLOW_OPTS $VALIDATOR_OPTS"
+
