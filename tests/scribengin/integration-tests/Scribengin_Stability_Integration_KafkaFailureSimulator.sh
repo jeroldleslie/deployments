dbed855985c859c502dc1985cfc89c95198a167c
KingOfPoptart <richard.c.duarte@gmail.com>
2015-10-09 13:55:29 +0100

    Adding Kafka Failure test for Scribengin

---
+++ tests/scribengin/integration-tests/Scribengin_Stability_Integration_KafkaFailureSimulator.sh
@@ -0,0 +1,32 @@
+#SCRIPT_DIR is the directory this script is in.  Allows us to execute without relative paths
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#This Script will set root directory and Neverwinter_home
+source $SCRIPT_DIR/setupEnvironment.sh $@
+
+
+
+#Run failure simulator in the background
+ssh -o StrictHostKeyChecking=no neverwinterdp@hadoop-master "cd /opt/cluster && mkdir -p /opt/neverwinterdp/scribengin/tools/kafka/junit-reports"
+ssh -o StrictHostKeyChecking=no neverwinterdp@hadoop-master "cd /opt/cluster &&                                     \
+                                  python clusterCommander.py --debug kafkafailure                                    \
+                                  --wait-before-start 30 --failure-interval 30 --kill-method shutdown                 \
+                                  --servers-to-fail-simultaneously 1                                                 \
+                                  --junit-report /opt/neverwinterdp/scribengin/tools/kafka/junit-reports/kafkaFailureReport.xml" &
+FAIL_SIM_PID=$!
+
+ssh -o StrictHostKeyChecking=no neverwinterdp@hadoop-master "cd /opt/cluster && \
+                                    python clusterCommander.py  monitor --update-interval 10 " &
+MONITOR_PID=$!
+
+
+#Run kafkaStabilityCheckTool
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/neverwinterdp/ &&     \
+                              ./dataflow/log-sample/bin/run-dataflow-chain.sh --num-of-message=1000000  --generator-max-wait-time=10000 \ 
+                              --generator-send-period=1  --max-run-time=604800000 --junit-report junit-reports/ScribenginKafkaFailureStability.xml"
+
+
+#Get results
+scp -o stricthostkeychecking=no neverwinterdp@hadoop-master:/opt/neverwinterdp/scribengin/tools/kafka/junit-reports/*.xml $TEST_RESULTS_LOCATION
+
+kill -9 $FAIL_SIM_PID $MONITOR_PID
