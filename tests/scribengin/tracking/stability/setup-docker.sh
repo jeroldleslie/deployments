4842d39b569fb34f78583c677896981d306cff05
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2015-10-21 13:25:59 +0530

    modified test scripts

--- tests/scribengin/tracking/stability/setup-docker.sh
+++ tests/scribengin/tracking/stability/setup-docker.sh
@@ -2,8 +2,9 @@
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT=$SCRIPT_DIR/../../../..
+INVENTORY=/tmp/scribengininventory
 
 clusterCommander="$ROOT/tools/cluster/clusterCommander.py"
 
 $ROOT/docker/scribengin/docker.sh cluster --clean-containers --run-containers 
-
+$clusterCommander ansible --write-inventory-file --inventory-file $INVENTORY
