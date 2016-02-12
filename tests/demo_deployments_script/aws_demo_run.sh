70e8256619135f79bcc0d0d5eb5bd35386ebf560
Peter Jerold Leslie <jeroldleslie@gmail.com>
2016-02-12 13:27:08 +0530

    added force-stop option, and bug fixes

--- tests/demo_deployments_script/aws_demo_run.sh
+++ tests/demo_deployments_script/aws_demo_run.sh
@@ -3,6 +3,7 @@ counter=0
 gripper_host=$1
 echo $gripper_host
 key_secret="OvQVG3oQGsC07YFeaJLrXViNNBmj28f6X5Yk0ORKGFt1oMBEiHDQlGQoCxhyjTv9ub4BwI6FIe9Y"
+type="scribengin_odyssey_demo"
 while true; do
    counter=`expr $counter + 1`
    ts=$(date +%s)
@@ -10,13 +11,13 @@ while true; do
    date_ts=$(date)
    echo 'date_ts' $date_ts
    hmac="$(printf "$ts" | openssl sha1 -binary -hmac "$key_secret" | base64)"
-   curl  -X POST $gripper_host/message/test-topic1/sync -d '{"event": {"id":'$counter',"type": "demonow-did-something","ts": '$ts', "hmac": "'$hmac'" }} '
-   echo 'Sending message : {"event": {"id":'$counter',"type": "demonow-did-something","ts": '$ts',"date":"'$date_ts'", "hmac": "'$hmac'" }} '
+   curl  -X POST $gripper_host/message/test-topic1/sync -d '{"event": {"id":'$counter',"type":"'$type'","ts": '$ts', "hmac": "'$hmac'" }} '
+   echo 'Sending message : {"event": {"id":'$counter',"type":"'$type'","ts": '$ts',"date":"'$date_ts'", "hmac": "'$hmac'" }} '
 #   sleep 1
 done
 
 # Run this file multiple times parallely
-#for((i=1;i<100;i++)); do nohup ./aws_demo_run.sh ec2-52-32-241-54.us-west-2.compute.amazonaws.com > aws_demo_run.log 2>&1& & done
+#for((i=1;i<100;i++)); do nohup ./aws_demo_run.sh ec2-52-36-174-224.us-west-2.compute.amazonaws.com & done
 
 # Using GNU Parallel in MAC OS or any other machine
 #seq 1 10000 | parallel -j20 ./aws_demo_run.sh
--- tests/demo_deployments_script/deploy_odyssey_scribengin_demo.sh
+++ tests/demo_deployments_script/deploy_odyssey_scribengin_demo.sh
@@ -14,15 +14,14 @@ $awsHelper ansibleinventory --identifier odyssey,neverwinterdp
 echo "Update hosts file on all instances***************************"
 $awsHelper updateremotehostfile -i odyssey,neverwinterdp -k /home/neverwinterdp/test.pem 
 
-
 #Install, configure and start all services
 echo "Install, configure and start all services************************"
 $serviceCommander -e "kafka,zookeeper" --force-stop --clean --configure --start 
-$serviceCommander -e "gripper" --install --configure --start 
-$serviceCommander -e "load_balancer"  --install --configure --start 
-$serviceCommander -e "odyssey_elasticsearch" --install --configure --start
+$serviceCommander -e "gripper" --force-stop --install --configure --start 
+$serviceCommander -e "load_balancer"  --force-stop --install --configure --start 
+$serviceCommander -e "odyssey_elasticsearch" --force-stop --install --configure --start
 
-$serviceCommander -e "storm_zookeeper" --install --configure --start
-$serviceCommander -e "storm_nimbus" --install --configure --start
-$serviceCommander -e "storm_supervisor" --install --configure --start
-$serviceCommander -e "storm_code" --install --configure --start
+$serviceCommander -e "storm_zookeeper" --force-stop --install --configure --start --clean
+$serviceCommander -e "storm_nimbus" --force-stop --install --configure --start
+$serviceCommander -e "storm_supervisor" --force-stop --install --configure --start
+$serviceCommander -e "storm_code" --force-stop --install --configure --start
