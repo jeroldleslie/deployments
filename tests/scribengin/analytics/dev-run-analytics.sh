bc99c3fc5474c333987af4c446d8587d87a99e2b
Tuan Nguyen <tuan08@gmail.com>
2016-03-25 00:58:02 -0400

    reconfigure kibana

--- tests/scribengin/analytics/dev-run-analytics.sh
+++ tests/scribengin/analytics/dev-run-analytics.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+
+COMMAND=$1
+shift
+
+if [ "$COMMAND" = "" ] ; then
+  echo ""
+  echo "Available commands"
+  echo "  quick  :  Run a quick analytics test"
+  echo "  monitor:  Run a long test  with min/max visit time that simulate the real web environment"
+  exit 0
+fi
+
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ROOT=$SCRIPT_DIR/../../..
 
-INVENTORY="/tmp/cluster-stability.inventory"
+INVENTORY="$HOME/inventory"
 
 serviceCommander="$ROOT/tools/serviceCommander/serviceCommander.py"
 statusCommander="$ROOT/tools/statusCommander/statusCommander.py"
@@ -30,15 +42,18 @@ $SHELL vm info
 #################################################################################################################################
 #Launch Dataflow                                                                                                                #
 #################################################################################################################################
+if [ "$COMMAND" = "quick" ] ; then
+  ANALYTICS_OPTS="\
+    --zk-connect zookeeper-1:2181 --hadoop-master-connect hadoop-master \
+    --generator-odyssey-num-of-events 10000 --generator-web-num-of-events 100000 \
+    --dataflow-num-of-workers 5 --dataflow-num-of-executor-per-worker 4 --wait-for-input"
+  time $NEVERWINTERDP_BUILD/dataflow/analytics/bin/run-analytics.sh $ANALYTICS_OPTS
+elif [ "$COMMAND" = "monitor" ] ; then
+  ANALYTICS_OPTS="\
+    --zk-connect zookeeper-1:2181 --hadoop-master-connect hadoop-master \
+    --generator-odyssey-num-of-events 10000 --generator-web-num-of-events 10000000 \
+    --dataflow-num-of-workers 5 --dataflow-num-of-executor-per-worker 4 \
+    --generator-web-num-of-threads 5 --generator-web-max-visit-time 40000 --generator-web-min-visit-time 5000 --wait-for-input"
+  time $NEVERWINTERDP_BUILD/dataflow/analytics/bin/run-analytics.sh $ANALYTICS_OPTS
+fi
 
-ANALYTICS_OPTS="\
-  --zk-connect zookeeper-1:2181 --hadoop-master-connect hadoop-master \
-  --generator-odyssey-num-of-events 10000 --generator-web-num-of-events 100000 \
-  --dataflow-num-of-workers 5 --dataflow-num-of-executor-per-worker 3"
-
-function runTest() {
-  time $NEVERWINTERDP_BUILD/dataflow/analytics/bin/run-analytics.sh $ANALYTICS_OPTS 
-}
-
-
-runTest
